a1e170b260b32f90ef3ece667d55179a9fc5da39
ihimawan <indri.himawan18@yahoo.com>
2017-04-05 19:59:11 -0400

    jersey configuration

--- MyWebApp/src/main/java/com/ih/rest/jersey/JerseyApp.java
+++ MyWebApp/src/main/java/com/ih/rest/jersey/JerseyApp.java
@@ -3,9 +3,13 @@ package com.ih.rest.jersey;
 import javax.ws.rs.ApplicationPath;
 
 import org.glassfish.jersey.server.ResourceConfig;
+import org.glassfish.jersey.server.filter.RolesAllowedDynamicFeature;
 
 @ApplicationPath("/jersey")
 public class JerseyApp extends ResourceConfig{
 
+	public JerseyApp(){
+		register(RolesAllowedDynamicFeature.class);
+	}
 	
 }
