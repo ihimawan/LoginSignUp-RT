8ed2b36ebdbaffcdb3e2278f0181d93763fc80ba
ihimawan <indri.himawan18@yahoo.com>
2017-04-05 20:12:48 -0400

    added user principal and security context.

---
+++ MyWebApp/src/main/java/com/ih/rest/jersey/user/User.java
@@ -0,0 +1,59 @@
+package com.ih.rest.jersey.user;
+
+import java.security.Principal;
+import java.util.List;
+
+public class User implements Principal{
+
+	private String id, firstName, lastName, login, email, password;
+    private List<String> role;
+    
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getLogin() {
+		return login;
+	}
+	public void setLogin(String login) {
+		this.login = login;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public List<String> getRole() {
+		return role;
+	}
+	public void setRole(List<String> role) {
+		this.role = role;
+	}
+	public String getName() {
+		return firstName + " " + lastName;
+	}
+    
+    
+	
+}
